f850fd8c95a4238ad8cb54f627d6f0aed6cf51c6
Brandon West <brawest@gmail.com>
2019-02-14 13:21:56 -0800

    formatting

--- rss/dbconfig.js
+++ rss/dbconfig.js
@@ -10,29 +10,34 @@ const client = new AWS.SecretsManager();
 client.getSecretValue = util.promisify(client.getSecretValue);
 
 module.exports = (async () => {
-  let data;
-  try {
-    data = await client.getSecretValue({
-      SecretId: secretName
-    });
-  } catch (err) {
-    console.error(err);
-    throw (err.message);
-  }
-
-  secret = data.SecretString;
-
-  connections = {
-    production: {
-      client: 'mysql',
-      connection: {
-        database: 'rss',
-        host: JSON.parse(secret).host,
-        user: JSON.parse(secret).username,
-        password: JSON.parse(secret).password
-      }
+    let data;
+    try {
+        data = await client.getSecretValue({
+        SecretId: secretName
+        });
+    } catch (err) {
+        console.error(err);
+        throw (err.message);
     }
-  }
 
-  return connections;
+    secret = data.SecretString;
+
+    connections = {
+        development: {
+            client: 'sqlite3',
+            connection: {
+                filename: 'development.db'
+            }
+        },
+        production: {
+            client: 'mysql',
+            connection: {
+                database: 'rss',
+                host: JSON.parse(secret).host,
+                user: JSON.parse(secret).username,
+                password: JSON.parse(secret).password
+            }
+        }
+    }
+    return connections;
 })();
